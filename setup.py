af1f701f1794b6195c6b733f75211b09f2f8a700
Ryan Williams <ryan@runsascoded.com>
2023-08-05 11:33:41 -0400

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name="stdlb",
-    version="0.0.3",
+    version="0.0.4",
     packages=["stdlb"],
     license="MIT",
     author="Ryan Williams",
     author_email="ryan@runsascoded.com",
     author_url="https://github.com/ryan-williams",
+    url="https://github.com/runsascoded/stdlb",
     description="Wildcard-import the Python standard library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
